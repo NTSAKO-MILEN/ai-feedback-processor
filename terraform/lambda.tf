7a075220cc63f2cf2b356446d80ded2280369400
NTSAKO MILEN <ntsakomilen3@gmail.com>
2025-07-21 22:29:47 +0000

    defined the Lambda function and its deployment setup.

---
+++ terraform/lambda.tf
@@ -0,0 +1,24 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda/main.py"
+  output_path = "${path.module}/../lambda/main.zip"
+}
+
+resource "aws_lambda_function" "feedback_handler" {
+  function_name = "${var.project_name}-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "main.lambda_handler"
+  runtime       = "python3.11"
+  filename      = data.archive_file.lambda_zip.output_path
+  timeout       = 10
+
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      BUCKET_NAME = var.s3_bucket_name
+    }
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.lambda_policy_attachment]
+}
